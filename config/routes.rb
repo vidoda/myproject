d51754a2de81c220e9fb2ade708eb71f229e1f74
vidoda <dauce@ua.pt>
2023-02-02 22:16:12 +0000

    my firs commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #render html: "hello, world!"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
